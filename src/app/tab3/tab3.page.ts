1ddee8801d7df17293dfbfbb4f40b6d368b0b6db
Fate Jacobson <82694104+FateJacobson@users.noreply.github.com>
2022-02-19 17:03:36 -0800

    Adjust Front End Pages. Add Get Function

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  runHttp() {
+    this.http.get('http://brysonreese.duckdns.org:5000/api/v1/users')
+      .subscribe(data => {
+        console.log(data);
+      })
+  }
 
 }
