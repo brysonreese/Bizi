fbe592b99bcdb31e79da0bce5a50d45d25c8763b
AustenChu <Austenchu8@gmail.com>
2022-02-23 13:46:06 -0800

    Login Page and Routing V1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
+const routes: Routes = [    
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
-];
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)   
+  },   
+    {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)   
+  },      
+  {
+    path : '',
+    redirectTo : 'login',
+    pathMatch : 'full'   
+  } ];
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
