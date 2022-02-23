873a604191dadf4ee5cf4fc492f27985e7ca8549
AustenChu <Austenchu8@gmail.com>
2022-02-23 15:35:39 -0800

    Login -> Register Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,28 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [    
+
   {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)   
-  },   
-    {
-    path: 'tabs',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)   
-  },      
-  {
-    path : '',
-    redirectTo : 'login',
-    pathMatch : 'full'   
-  } ];
+  path: 'login',
+  loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)   
+},   
+
+{
+  path: 'tabs',
+  loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)   
+},      
+
+{
+  path : '',
+  redirectTo : 'login',
+  pathMatch : 'full'   
+},   {
+    path: 'register',
+    loadChildren: () => import('./auth/register/register.module').then( m => m.RegisterPageModule)
+  }
+
+];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
