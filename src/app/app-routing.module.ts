728f1eeaeafb34f8908711493dc9685b677454a2
Colton Morley <coltonmorley@Coltons-MacBook-Air.local>
2022-03-09 14:53:35 -0800

    Mostly Tab5 Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,6 +20,22 @@ const routes: Routes = [
 },   {
     path: 'register',
     loadChildren: () => import('./auth/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'new-request',
+    loadChildren: () => import('./new-request/new-request.module').then( m => m.NewRequestPageModule)
+  },
+  {
+    path: 'request-success',
+    loadChildren: () => import('./request-success/request-success.module').then( m => m.RequestSuccessPageModule)
+  },
+  {
+    path: 'coming-up',
+    loadChildren: () => import('./coming-up/coming-up.module').then( m => m.ComingUpPageModule)
+  },
+  {
+    path: 'completed-expenses',
+    loadChildren: () => import('./completed-expenses/completed-expenses.module').then( m => m.CompletedExpensesPageModule)
   }
 
 ];
